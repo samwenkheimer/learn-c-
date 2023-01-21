8c02a6e58cf4fcfb143b1822327b1183d5ecce8f
samwenkheimer <sam.wenkheimer@yahoo.com>
2023-01-20 18:22:15 -0800

    user input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+Console.WriteLine ("Enter number:");
+
+// READ LINE IS THE FUCKING USER INPUT AND WE STORE IT IN NUMBER 
+string? number = Console.ReadLine();
+int x;
+
+int.TryParse(number, out x);
+
+for (int i = 0; i < x; i++) {
+    Console.WriteLine(i);
+}
+    
+    
+
+
